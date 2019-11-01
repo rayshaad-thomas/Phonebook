6606ff5ff8275f67e4c79e3cf599f1b0c15fe3e4
rayshaad-thomas <56983754+rayshaad-thomas@users.noreply.github.com>
2019-11-01 15:15:32 -0500

    Update Business.java

--- Business.java
+++ Business.java
@@ -1,30 +1,15 @@
-/*
- * Rayshaad Thomas
- * 892564854
- * Lab Section 1
- * Lab Assignment 5
- * Submission time: 6:36
- */
 package phonebookproject;
-
 /**
- *
  * @author rtho137
  */
 public class Business extends Contact 
 {
     private int zip;
-    
     public Business(String name, long phone, int zip) 
     {
         super(name, phone);
         this.zip = zip;
     }
-    
-    /**
-     *
-     * @return
-     */
     @Override
     public String getInfo()
     {
