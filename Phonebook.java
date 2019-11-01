a24134c92dcf908148b3b3d1e2609848d16c4524
rayshaad-thomas <56983754+rayshaad-thomas@users.noreply.github.com>
2019-11-01 15:22:28 -0500

    Update Phonebook.java

--- Phonebook.java
+++ Phonebook.java
@@ -1,70 +1,58 @@
-/*
- * Rayshaad Thomas
- * 892564854
- * Lab Section 1
- * Lab Assignment 5
- * Submission time: 6:36
- */
 package phonebookproject;
-
 import java.util.ArrayList;
 import java.util.Collections;
-
 /**
- *
- * @author rtho137
+ * @author rayshaadthomas
  */
-public class Phonebook
+public class Phonebook 
 {
-    private ArrayList <Person> person = new ArrayList<>();
-    private ArrayList <Business> business = new ArrayList<>();
-    
-    public Phonebook()
+    private ArrayList <Person> person = new ArrayList <> ();
+    private ArrayList <Business> business = new ArrayList <> ();
+    // blank constructor
+    public Phonebook ()
     {
         
     }
-    
-    public void addPerson(String name, long phone, String relationship)
+    // The method adds a new Person element to the person array.
+    public void addPerson (String name, long phone, String relationship) 
     {
         Person newPerson = new Person(name, phone, relationship);
         person.add(newPerson);
     }
-    
-    public void addBusiness(String name, long phone, int zip)
+    // The method adds a new Business element to the business array.
+    public void addBusiness (String name, long phone, int zip) 
     {
         Business newBusiness = new Business(name, phone, zip);
         business.add(newBusiness);
-        
     }
-    
-    public void sortContacts()
+     public void addBusiness (Business business1) 
+     {
+         business.add(business1);
+     }
+    // The method sorts both the person and business array.
+    public void sortContacts () 
     {
         Collections.sort(person);
         Collections.sort(business);
     }
-    
-    public void printContacts()
+    public void printContacts () 
     {
         sortContacts();
-        System.out.printf("%-20s %-20s %-20s\n","Person Name","Phone","Relationship");
-        System.out.println("------------------------------------------------------");
-        
+        System.out.println("-------------------------------------------------------");
+        System.out.printf("%-20s %-20s %-20s\n", "Name", "Phone", "Relationship");
+        System.out.println("-------------------------------------------------------\n");
+        // The loop searches for Contacts in the person array.
         for (Contact personName : person)
         {
             System.out.println(personName.getInfo());
         }
-        
-        System.out.println("------------------------------------------------------");
-        System.out.printf("%-20s %-20s %-20s\n","Business Name","Phone","Zip");
-        System.out.println("------------------------------------------------------");
-        
-        for (Contact businessName : business)
+        System.out.println("\n------------------------------------------------------"); 
+        System.out.printf("%-20s %-20s %-20s\n", "Name", "Phone", "Zip");
+        System.out.println("------------------------------------------------------\n");
+        // The loop searches for Contacts in the business array.
+        for (Contact businessName : business) 
         {
             System.out.println(businessName.getInfo());
-        }
-        
-        System.out.println("------------------------------------------------------");
+        }          
     }
-    
-    
 }
