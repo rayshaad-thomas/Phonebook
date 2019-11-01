1426835cac2cf76a1a001f8761098a0a2352a925
rayshaad-thomas <56983754+rayshaad-thomas@users.noreply.github.com>
2019-11-01 15:24:33 -0500

    Update Business.java

--- Business.java
+++ Business.java
@@ -2,14 +2,20 @@ package phonebookproject;
 /**
  * @author rayshaadthomas
  */
-public class Business extends Contact 
+// This class is specifically for contacts that are people. Includes their zip
+// code.
+public class Business extends Contact
 {
-    private int zip;
-    public Business(String name, long phone, int zip) 
+    // Since the class is for business contacts, we must add the zip code.
+    private int zip; 
+    // Constructor class that initializes all of the variables.
+    public Business (String name, long phone, int zip)
     {
         super(name, phone);
         this.zip = zip;
     }
+    // The method overrides the getInfo() method in the Contact class and adds 
+    //  the zip code
     @Override
     public String getInfo()
     {
