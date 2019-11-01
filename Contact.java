bff1af47898a86843dd429546a8a238dd6fe47c4
rayshaad-thomas <56983754+rayshaad-thomas@users.noreply.github.com>
2019-11-01 15:17:44 -0500

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,38 +1,22 @@
-/*
- * Rayshaad Thomas
- * 892564854
- * Lab Section 1
- * Lab Assignment 5
- * Submission time: 6:36
- */
 package phonebookproject;
-
 /**
- *
- * @author rtho137
+ * @author rayshaadthomas
  */
 public class Contact implements Comparable<Contact>
 {
     public String name;
     public long phone;
-    
     public Contact(String name, long phone)
-        {
-            this.name = name;
-            this.phone = phone;
-        }
-    
-    
+    {
+        this.name = name;
+        this.phone = phone;
+    }
     public String getInfo()
     {
         return String.format("%-20s %-20d", name, phone);
-    }
-    
-    
-    
+    } 
     public int compareTo(Contact other)
     {
         return name.compareTo(other.name);
     }
-    
 }
